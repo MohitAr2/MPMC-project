49cb5c0f0decfa94d5ed5b5e2bb6c33ef6af38a9
Akshit Sinha <akshitsinha@outlook.com>
2023-11-22 00:27:21 +0530

    Fix formatting and add chat history

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  serverRuntimeConfig: {
-    headers: (req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-      next();
-    },
-  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "tailwindcss/tailwind.css";
-import axios from 'axios';
+import axios from "axios";
 
 const IndexPage = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [response, setResponse] = useState('');
+  const [inputValue, setInputValue] = useState("");
+  const [response, setResponse] = useState("");
+  const [chatHistory, setChatHistory] = useState<string[]>([]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -14,10 +15,13 @@ const IndexPage = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:5000/answer', {
+      const res = await axios.post("http://localhost:5000/answer", {
         prompt: inputValue,
       });
+
       setResponse(res.data);
+      setChatHistory((prevHistory) => [...prevHistory, inputValue, res.data]);
+      setInputValue("");
     } catch (error) {
       console.error(error);
     }
@@ -26,12 +30,36 @@ const IndexPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-4">Minemaw Chatbot</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+      <h1 className="text-lg font-bold mb-4">
+        AI based law chatbot to respond to queries related to the Indian mining
+        industries
+      </h1>
+      <div className="flex flex-col items-start justify-start w-3/4 h-96 bg-white border border-gray-300 rounded-lg p-4 overflow-auto">
+        <div className="flex flex-col space-y-2">
+          {chatHistory.map((message, index) => (
+            <div className="flex flex-row items-center" key={index}>
+              {index % 2 === 0 ? (
+                <div className="w-4 h-4 bg-blue-400 rounded-full"></div>
+              ) : (
+                <div className="w-4 h-4 bg-yellow-400 rounded-full"></div>
+              )}
+              <p
+                className={`ml-2 text-lg ${
+                  index % 2 === 1 ? "text-black" : "text-gray-600"
+                }`}
+              >
+                {message}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <form onSubmit={handleSubmit} className="flex flex-row items-center mt-4">
         <input
           type="text"
           value={inputValue}
           onChange={handleInputChange}
-          className="border border-gray-300 rounded-l px-4 py-2 focus:outline-none mb-2"
+          className="border border-gray-300 rounded-l px-4 py-2 focus:outline-none"
         />
         <button
           type="submit"
@@ -40,11 +68,6 @@ const IndexPage = () => {
           Send
         </button>
       </form>
-      {response && (
-        <div className="mt-4 p-4 bg-gray-200 rounded">
-          <p>{response}</p>
-        </div>
-      )}
     </div>
   );
 };
