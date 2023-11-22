bc1a56acf83916be08ac7aeb361b7ce20c9c3427
Mohit <119338819+MohitAr2@users.noreply.github.com>
2023-11-22 17:30:53 +0530

    Update features

--- features.py
+++ features.py
@@ -4,10 +4,14 @@ from gtts import gTTS
 import speech_recognition as sr
 import pygame # for audio playback
 import fitz  # PyMuPDF
-from langid import classify # for multiple langs not using here show example of one them tamil pls
+from langid import classify
+from googletrans import Translator # for multiple langs not using here show example of one them tamil pls
 def text_to_speech(text, language='en'):
     tts = gTTS(text=text, lang=language, slow=False)
-    tts.save("output.mp3")#maybe problemv where it'll save the file in the same directory as the code
+    tts.save("output.mp3")
+    #maybe problemv where it'll save the file 
+    #in the same directory as the code 
+    # what if a combination of mulotiple language is spoken ?
 def speech_to_text(language='en'):
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
@@ -30,16 +34,12 @@ def pdf_to_text(pdf_path)->str:
         page = doc[page_num]
         text += page.get_text()
     return text
-#here fro windows so 
-#file_path = r'C:\Users\91951\OneDrive\Desktop\MPMCs\nameoffile.pdf'
-#pdf_to_text(file_path) to convert obviously
-text_input = "Hello, how are you?"
-language_input = 'en'  # Choose from 'en', 'hi', 'ta', 'te', 'kn' 
-#this part is to be recognized or not ? 
-text_to_speech(text_input, language=language_input)
-text = speech_to_text(language=language_input)
-# For PDF-to-text
-pdf_path = r'C:\\Users\\91951\\OneDrive\Desktop\\MPMCs\\devsfile.pdf'  # Replace with your PDF file path
-pdf_text = pdf_to_text(pdf_path)
-print(pdf_text)
-#more to add are like if someone wants the bot to read all the stuff inpdf in a simplified text 
+def detect_language(text):
+    # Language detection using langid
+    lang, _ = classify(text)
+    return lang
+def translate_text(text, target_lang='en'):
+    # Google Translate API for translation
+    translator = Translator()
+    translated_text = translator.translate(text, dest=target_lang)
+    return translated_text.text
